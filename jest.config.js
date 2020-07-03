0ffaae695d833369acddc62cbea9a153898f8f4a
Diego Rugue <diegorugue@gmail.com>
2020-07-02 22:39:13 -0300

    refactor: ignore main on coverage test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/{!(index),}.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/{!(index),}.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
