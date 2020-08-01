551f8bc4d3dc96c19875e155e2eaa25825c8baa5
Diego Rugue <diegorugue@gmail.com>
2020-08-01 15:51:37 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -6,13 +6,13 @@ const makeSut = (): RequiredFieldValidation => {
 }
 
 describe('Required Field Validation', () => {
-  test('Should return InvalidParamError if EmailValidator returns false', () => {
+  test('Should return MissingParamError if RequiredFieldValidation returns false', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
-  test('Should not return if EmailValidator succeeds', () => {
+  test('Should not return if RequiredFieldValidation succeeds', () => {
     const sut = makeSut()
     const error = sut.validate({ field: 'any_field' })
     expect(error).toBeFalsy()
