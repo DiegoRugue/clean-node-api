9ea69e8d23f8981f3a86f742bb55dc80aa6cbd85
Diego Ruguê <diegorugue@gmail.com>
2020-08-30 14:26:44 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-code-api',
-  port: process.env.PORT || '5050'
+  port: process.env.PORT || '5050',
+  salt: +process.env.SALT || 8,
+  jwtSecret: process.env.SECRET || 'GG%EASY'
 }
